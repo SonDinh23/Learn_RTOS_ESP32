7495aac769cedacdd2782183d7e23b3dde698b11
Dinhquangson23 <dinhson8878@gmail.com>
2023-02-09 21:35:38 +0700

    update1

---
+++ src/Intro.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+
+
+void Task1(void* pvParameters);
+void Task2(void* pvParameters);
+
+void Task1(void* Paramter) {
+  for(;;) {
+    Serial.println("Task1");
+    vTaskDelay(1000);
+  }
+}
+
+void Task2(void* Paramter) {
+  for(;;) {
+    Serial.println("Task2");
+    vTaskDelay(1000);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  xTaskCreatePinnedToCore(Task1,"Task1",1024,NULL,1,NULL,0);
+  xTaskCreatePinnedToCore(Task2,"Task2",1024,NULL,1,NULL,1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  vTaskDelete(NULL);
+}
